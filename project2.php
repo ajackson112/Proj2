e174eff34d9428a2a587a8d0b777a22d71bce6d0
Alex Jackson <ajackson112@student.gsu.edu>
2015-10-18 13:16:26 -0400

    Updated files
    Got card back and background to pass to project2.php

--- project2.php
+++ project2.php
@@ -1,24 +1,56 @@
+<!DOCTYPE html>
 <html>
 <head>
-<title>SOLITAIRE GAME</title>
-<link rel="stylesheet" type="text/css" href="new.css" />
-</head>
-<body>
-<div id="head">
-   <h1> SOLITAIRE </h1>
-      <div id="menubar">
-	     <a href="project1.html">START A NEW GAME</a></li></li>
-      </div>
-</div>
-
 <?php
    $background = $_POST['background'];
    $card = $_POST['card'];
-   
-
-   echo "<body style='background-image: url($background); background-repeat:no-repeat; background-size:100%'>";
-   echo "<img src='$card'>";
 ?>
-<body style='background-image: url(); background-repeat:no-repeat; background-size:100%'><body style='background-image: url()'>
+<style>
+body{
+	background:url("<?php print $_POST["background"]?>");	 	
+	background-size:cover;
+}
+</style>
+<title>ROBOT SOLITAIRE GAME</title>
+<link rel="stylesheet" type="text/css" href="project2.css" />
+</head>
+
+<body>
+
+<div id="head">
+<h1> SOLITAIRE </h1>
+<div id="menubar">
+
+		<ul>
+			<li><a href="project1.html">START A NEW GAME</a></li></li>
+		</ul>
+		
+	</div>
+</div>
+
+<table class="center">
+<tr>
+<td><a href="project2m.html"><img class="images" src=<?php print $_POST["card"]?>></a> </td>
+<td class="blank"></td>
+<td class="blank"></td>
+<td><img class="images" src="two.png" > </td>
+<td><img class="images" src="ace.png" > </td>
+<td><img class="images" src="jacks.png" > </td>
+<td><img class="images" src="king.png" > </td>
+</tr>
+<tr>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+<td><img class="images" src="<?php print $_POST["card"]?>" > </td>
+</tr>
+</table>
+
+
 </body>
+<h6>ROBOT SOLITAIRE</h6>
+
 </html>
